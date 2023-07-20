7038c5ec5efad7925b0fc9561533d75ee488fac1
abdullahqusim <abdullahqusim@gmail.com>
2023-07-20 12:34:11 +0100

    feat: created pages and practices map

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.astro',
+		'../components/**/*.astro',
+		'../components/**/*.astro',
+	],
 	theme: {
 		extend: {},
 		fontFamily: {
